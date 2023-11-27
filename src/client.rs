155560f129da98e2e839e4f1c32d12b422a1964a
jiaoshijie <jiao1751959040@gmail.com>
2023-11-27 23:50:35 +0800

    try ratatui in client.rs

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("hello client");
+use crossterm::{
+    cursor,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{backend::CrosstermBackend as Beckend, widgets::Paragraph};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let mut terminal = ratatui::terminal::Terminal::new(Beckend::new(std::io::stdout())).unwrap();
+
+    // enter raw-mode and Alternative-screen
+    crossterm::terminal::enable_raw_mode().unwrap();
+    crossterm::execute!(std::io::stdout(), EnterAlternateScreen, cursor::Hide).unwrap();
+
+    terminal
+        .draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(Paragraph::new("Hello World!"), area);
+            frame.set_cursor(0, 0);
+        })
+        .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    // leave raw-mode and Alternative-screen
+    crossterm::execute!(std::io::stdout(), LeaveAlternateScreen, cursor::Show).unwrap();
+    crossterm::terminal::disable_raw_mode().unwrap();
 }
