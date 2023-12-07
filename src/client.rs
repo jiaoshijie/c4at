26115a1208a890c265c88775555c0a7aba8fa70b
jiaoshijie <jiao1751959040@gmail.com>
2023-12-07 09:45:09 +0800

    sync

--- src/client.rs
+++ src/client.rs
@@ -3,14 +3,24 @@ use crossterm::{
     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ratatui::{backend::CrosstermBackend as Beckend, widgets::Paragraph};
+use tokio::net::TcpStream;
+
+type Result<T> = std::result::Result<T, ()>;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
-    let mut terminal = ratatui::terminal::Terminal::new(Beckend::new(std::io::stdout())).unwrap();
+async fn main() -> Result<()> {
+    let mut terminal =
+        ratatui::terminal::Terminal::new(Beckend::new(std::io::stdout())).map_err(|err| {
+            eprintln!("ERROR: create terminal failed: {err}");
+        })?;
 
     // enter raw-mode and Alternative-screen
-    crossterm::terminal::enable_raw_mode().unwrap();
-    crossterm::execute!(std::io::stdout(), EnterAlternateScreen, cursor::Hide).unwrap();
+    crossterm::terminal::enable_raw_mode().map_err(|err| {
+        eprintln!("ERROR: enable raw mode failed: {err}");
+    })?;
+    crossterm::execute!(std::io::stdout(), EnterAlternateScreen, cursor::Hide).map_err(|err| {
+        eprintln!("ERROR: enter alternative screen failed: {err}");
+    })?;
 
     terminal
         .draw(|frame| {
@@ -23,6 +33,12 @@ async fn main() {
     std::thread::sleep(std::time::Duration::from_secs(2));
 
     // leave raw-mode and Alternative-screen
-    crossterm::execute!(std::io::stdout(), LeaveAlternateScreen, cursor::Show).unwrap();
-    crossterm::terminal::disable_raw_mode().unwrap();
+    let _ =
+        crossterm::execute!(std::io::stdout(), LeaveAlternateScreen, cursor::Show).map_err(|err| {
+            eprintln!("ERROR: leave alternative screen failed: {err}");
+        });
+    crossterm::terminal::disable_raw_mode().map_err(|err| {
+        eprintln!("ERROR: disable raw mode failed: {err}");
+    })?;
+    Ok(())
 }
